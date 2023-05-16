51fc999a48106c1d7f60967de09fca3f10784e74
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-05-16 14:24:45 -0700

    prettier seeds

--- seeds/userSeeds.js
+++ seeds/userSeeds.js
@@ -1,30 +1,30 @@
-const faker = require('faker');
-const bcrypt = require('bcrypt');
-const User = require('../models/User');
+const faker = require("faker");
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
 
 const seedUsers = async () => {
-    const fakeUsers = [];
+  const fakeUsers = [];
 
-    for (let i = 0; i < 25; i++) {
-        const email = faker.internet.email();
-        const userName = email.split('@')[0];
-        const password = await bcrypt.hash('password123', 16);
+  for (let i = 0; i < 25; i++) {
+    const email = faker.internet.email();
+    const userName = email.split("@")[0];
+    const password = await bcrypt.hash("password123", 16);
 
-        try {
-        fakeUsers.push({
-            email,
-            userName,
-            password,
-        });
+    try {
+      fakeUsers.push({
+        email,
+        userName,
+        password,
+      });
     } catch (err) {
-        console.error(err);
+      console.error(err);
     }
-        try {
-            await User.bulkCreate(fakeUsers);
-        } catch (err) {
-            console.error(err);
-        }
+    try {
+      await User.bulkCreate(fakeUsers);
+    } catch (err) {
+      console.error(err);
     }
+  }
 };
 
 module.exports = seedUsers;
